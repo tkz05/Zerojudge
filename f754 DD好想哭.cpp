c39b6ee324cda9740af17bc02b5036164c147ea6
tkz05 <82510438+tkz05@users.noreply.github.com>
2021-05-08 17:40:42 +0800

    f54

--- f754 DD好想哭.cpp
+++ f754 DD好想哭.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int main()
 {
-    int M,N,K;
-    int count=1;
+    long long int M,N,K;
+    long long int count=1;
     cin>>N;
     while(N--)
     {
+        cin.tie(0);
+        ios::sync_with_stdio(false);
         cin>>M>>K;
         cout<<"Case "<<count++<<" :"<<endl;
-        for(int i=1; i<=K; i++)
+        long long int a=M/K+M%K;
+        long long int b=M/K;
+        for(long long int i=1; i<K; i++)
         {
-            if(i==K)
-                cout<<"第"<<i<<"位 : 拿"<<M/K+M%K<<"元並說DD! BAD!"<<endl;
-            else
-                cout<<"第"<<i<<"位 : 拿"<<M/K<<"元並說DD! BAD!"<<endl;
+            cout<<"第"<<i<<"位 : 拿"<<b<<"元並說DD! BAD!\n";
         }
+            cout<<"第"<<K<<"位 : 拿"<<a<<"元並說DD! BAD!\n";
     }
-
-
 }
